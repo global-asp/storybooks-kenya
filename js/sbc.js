74c1bbcdd9810fbbe57d16d4b4fba38bbef47f9c
dohliam <dohliam@gmail.com>
2018-04-07 07:44:13 -0700

    update site

--- js/sbc.js
+++ js/sbc.js
@@ -20,41 +20,55 @@ function playpause(n) {
   }
 }
 
-function switchlang(d,l) {
-  en = document.getElementsByClassName("en");
-  sw = document.getElementsByClassName("sw");
+function switchlang(o,c) {
+  options = o.split(",");
+  config = c.split(",");
+  from_lang = options[0];
+  to_lang = options[1];
+  primary = config[0];
+  secondary = config[1];
+  page_lang = config[2];
+  l1 = document.getElementsByClassName("l1");
+  l2 = document.getElementsByClassName("l2");
   def = document.getElementsByClassName("def");
-  btn_en = document.getElementsByClassName("lang-en");
-  btn_sw = document.getElementsByClassName("lang-sw");
-  if (l == "en") {
-    for (var i = 0; i < btn_en.length + 1; i++) {
-      sw[i].style.display = "none";
-      en[i].style.display = "block";
+  btn_lang1 = document.getElementsByClassName("lang-primary");
+  btn_lang2 = document.getElementsByClassName("lang-secondary");
+
+  if (to_lang == primary) {
+    for (var i = 0; i < btn_lang1.length; i++) {
+      l1[i].style.display = "block";
+      l2[i].style.display = "none";
       def[i].style.display = "none";
-      btn_en[i].innerHTML = d;
-      btn_sw[i].innerHTML = "sw";
-      btn_en[i].setAttribute("onclick", "switchlang('" + d + "','" + d + "')");
-      btn_sw[i].setAttribute("onclick", "switchlang('" + d + "','sw')");
+      if (page_lang != primary && page_lang != secondary) {
+        btn_lang1[i].innerHTML = page_lang;
+        btn_lang1[i].setAttribute("onclick", "switchlang('" + primary + "," + page_lang + "','" + c + "')");
+        btn_lang2[i].innerHTML = secondary;
+        btn_lang2[i].setAttribute("onclick", "switchlang('" + page_lang + "," + secondary + "','" + c + "')");
+      }
     }
-  } else if (l == "sw") {
-    for (var i = 0; i < btn_sw.length + 1; i++) {
-      en[i].style.display = "none";
-      sw[i].style.display = "block";
+  } else if (to_lang == secondary) {
+    for (var i = 0; i < btn_lang2.length; i++) {
+      l1[i].style.display = "none";
+      l2[i].style.display = "block";
       def[i].style.display = "none";
-      btn_en[i].innerHTML = "en";
-      btn_sw[i].innerHTML = d;
-      btn_sw[i].setAttribute("onclick", "switchlang('" + d + "','" + d + "')");
-      btn_en[i].setAttribute("onclick", "switchlang('" + d + "','en')");
+      if (page_lang != primary && page_lang != secondary) {
+        btn_lang1[i].innerHTML = primary;
+        btn_lang1[i].setAttribute("onclick", "switchlang('" + page_lang + "," + primary + "','" + c + "')");
+        btn_lang2[i].innerHTML = page_lang;
+        btn_lang2[i].setAttribute("onclick", "switchlang('" + secondary + "," + page_lang + "','" + c + "')");
+      }
     }
-  } else if (l == d) {
-    for (var i = 0; i < btn_sw.length + 1; i++) {
-      en[i].style.display = "none";
-      sw[i].style.display = "none";
+  } else {
+    for (var i = 0; i < btn_lang1.length; i++) {
+      l1[i].style.display = "none";
+      l2[i].style.display = "none";
       def[i].style.display = "block";
-      btn_en[i].innerHTML = "en";
-      btn_sw[i].innerHTML = "sw";
-      btn_en[i].setAttribute("onclick", "switchlang('" + d + "','en')");
-      btn_sw[i].setAttribute("onclick", "switchlang('" + d + "','sw')");
+      if (page_lang != primary && page_lang != secondary) {
+        btn_lang1[i].innerHTML = primary;
+        btn_lang1[i].setAttribute("onclick", "switchlang('" + page_lang + "," + primary + "','" + c + "')");
+        btn_lang2[i].innerHTML = secondary;
+        btn_lang2[i].setAttribute("onclick", "switchlang('" + page_lang + "," + secondary + "','" + c + "')");
+      }
     }
   }
 }
